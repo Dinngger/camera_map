35128d2710b0a3ac86a918f49a5327d51457abbe
Dinngger <814089338@qq.com>
2019-12-31 21:27:36 +0800

    light_detect

--- camera_ctl/GrabOpenCV.cc
+++ camera_ctl/GrabOpenCV.cc
@@ -1,4 +1,5 @@
 #include "CameraCtl.hpp"
+#include <vector>
 
 int main(int argc, char* argv[])
 {
@@ -6,42 +7,50 @@ int main(int argc, char* argv[])
     camCtl.startGrabbing();
     printf("Press any key to exit.\n");
     bool lowExposureTime = true;
+    std::vector<RotatedRect> rrects;
+    float mean_mean = 40;
     while (true) {
         Mat img = camCtl.getOpencvMat();
-        camCtl.setExposureTime(lowExposureTime ? 8000 : 1000);
+        camCtl.setExposureTime(lowExposureTime ? 7000 : 50);
         lowExposureTime = !lowExposureTime;
-        if (lowExposureTime) {
+		Mat grayImage;
+		Mat binary = img.clone();
+        cvtColor(img, grayImage, COLOR_BGR2GRAY);
+        Scalar meanValue;
+        meanValue = mean(grayImage);
+        printf("\rmean: %f\t", meanValue.val[0]);
+        bool lowExposure = meanValue.val[0] < mean_mean;
+        mean_mean = mean_mean * 0.95 + 0.05 * meanValue.val[0];
+
+        if (lowExposure) {
+            rrects.clear();
+            threshold(grayImage, binary, 253, 255, CV_THRESH_BINARY);
+ 
+            std::vector<std::vector<Point>> contours;
+            std::vector<Vec4i> hierarchy;
+            findContours(grayImage, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE, Point());
+
+            for (int i = 0;i < contours.size();i++)
+            {
+                //绘制轮廓的最小外结矩形
+                RotatedRect rrect = minAreaRect(contours[i]);
+                rrects.push_back(rrect);
+            }
+            // imshow("lowExposureImage", img);
+            // if (waitKey(1) > 0)
+            //     break;
+        } else {
+            for(int i = 0;i<rrects.size();i++){
+                Point2f corners[4];
+                rrects[i].points(corners);
+                for (int j = 0;j < 4;j++) {
+                    line(img, corners[j], corners[(j + 1) % 4], Scalar(0, 0, 255), 2);
+                }
+            }
             imshow("Image", img);
             if (waitKey(1) > 0)
                 break;
-        } 
-		Mat grayImage;
-		binary = img.clone();
-        cvtColor(img, grayImage, COLOR_BGR2GRAY);
-		threshold(img, binary, 30, 200.0, CV_THRESH_BINARY);
-		
-		vector<vector<Point>> contours;
-		vector<Vec4i> hierarchy;
-		findContours(img, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE, Point());
-		
-		vector<RotatedRect> rrects;
-		for (int i = 0;i < contours.size();i++)
-		{
-			//绘制轮廓的最小外结矩形
-			RotatedRect rrect = minAreaRect(contours[i]);
-			rrects.push_back(rrect);
-		}
-		for(int i = 0;i<rrects.size();i++){
-			Point2f corners[4];
-			rrects[i].points(corners);
-			for (int j = 0;j < 4;j++) {
-				line(img, corners[j], corners[(j + 1) % 4], Scalar(0, 0, 255), 2);
-			}
-		}
-		imshow("binary", binary);
-		imshow("img", img);
-		waitKey(0);
+        }
     }
-    waitKey(0);
     return 0;
 }
