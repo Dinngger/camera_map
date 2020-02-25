aa9c2618a2b808f0e845181eb08b61efe4e4c5ab
Cloud0723 <1586737008@qq.com>
2020-02-25 04:46:54 -0800

    add add_car

--- pose_solver/include/CarModule.hpp
+++ pose_solver/include/CarModule.hpp
@@ -5,7 +5,6 @@
 
 #ifndef __CAR_MODULE_HPP
 #define __CAR_MODULE_HPP
-
 #include <vector>
 #include <iostream>
 #include <fstream>
@@ -38,7 +37,7 @@ struct Car
 {
     int color=-1;
     int number=-1; // -1 means unknow
-    LightBar lb[4];
+    LightBar lb[8];
 };
 
 class CarModule
@@ -53,12 +52,62 @@ public:
     int find_light(LightBarP &lbp);
 };
 
-int CarModule::add_car(vector<Point3f> armor)
+int CarModule::add_car(vector<Point3f> &armor)
 {
     Car c;
     // TODO: create a car behind the armor.
     /**let other armors be the same size as this one.
     */
+
+    double x1,x2,y1,y2;
+    x1=armor[0].x;
+    x2=armor[1].x;
+    y1=armor[0].y;
+    y2=armor[1].y;
+    //Assume that distance(armor[0],armor[1])==long side
+    double length=400;//length of car
+    double len=sqrtf((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));//len of armor
+    double k_cos=(x2-x1)/sqrtf((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    double k_sin=(y2-y1)/sqrtf((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+    LightBar l7,l8;//the front lightbars
+    l7.p[0]=armor[0];
+    l7.p[1]=armor[3];
+    l8.p[0]=armor[1];
+    l8.p[1]=armor[2];
+    c.lb[0]=l7;
+    c.lb[1]=l8;
+
+    LightBar l1,l2;// the two back lightbars
+    l1.p[0]=armor[0]+Point3f(length*k_sin,-length*k_cos,0);
+    l1.p[1]=armor[3]+Point3f(length*k_sin,-length*k_cos,0);
+    l2.p[0]=armor[1]+Point3f(length*k_sin,-length*k_cos,0);
+    l2.p[1]=armor[2]+Point3f(length*k_sin,-length*k_cos,0);
+    c.lb[2]=l1;
+    c.lb[3]=l2;
+
+
+    LightBar l3,l4;//one of left or right
+    Point3f center1=(armor[0]+armor[1])/2+Point3f(length/2*k_sin,-length/2*k_cos,0)+Point3f(length/2*k_cos,-length/2*k_sin,0);
+    Point3f center2=(armor[2]+armor[3])/2+Point3f(length/2*k_sin,-length/2*k_cos,0)+Point3f(length/2*k_cos,-length/2*k_sin,0);
+    l3.p[0]=center1+Point3f(len*k_sin,-len*k_cos,0);
+    l3.p[1]=center1+Point3f(len*k_sin,-len*k_cos,0);
+    l4.p[0]=center2-Point3f(len*k_sin,-len*k_cos,0);
+    l4.p[1]=center2-Point3f(len*k_sin,-len*k_cos,0);
+    c.lb[4]=l3;
+    c.lb[5]=l4;
+
+
+    LightBar l5,l6;//another left or right
+    Point3f center3=(armor[0]+armor[1])/2+Point3f(length/2*k_sin,-length/2*k_cos,0)-Point3f(length/2*k_cos,-length/2*k_sin,0);
+    Point3f center4=(armor[2]+armor[3])/2+Point3f(length/2*k_sin,-length/2*k_cos,0)-Point3f(length/2*k_cos,-length/2*k_sin,0);
+    l5.p[0]=center3+Point3f(len*k_sin,-len*k_cos,0);
+    l5.p[1]=center3+Point3f(len*k_sin,-len*k_cos,0);
+    l6.p[0]=center4-Point3f(len*k_sin,-len*k_cos,0);
+    l6.p[1]=center4-Point3f(len*k_sin,-len*k_cos,0);
+    c.lb[6]=l5;
+    c.lb[7]=l6;
+
     cars.push_back(c);
     return 0;
 }
