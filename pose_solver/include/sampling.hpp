3fb085c193dd4096393ac6019badf35b63d97795
Enigmatisms <984041003@qq.com>
2020-03-01 23:39:39 +0800

    Changes in sampling and AimDeps.

--- pose_solver/include/sampling.hpp
+++ pose_solver/include/sampling.hpp
@@ -8,11 +8,19 @@
 #include <vector>
 #include <iostream>
 
-bool isLowExposure(cv::Mat &src){
+///TODO: 对于敌人是红色/蓝色可能的参数调整
+/**
+ * @brief 判断本帧是否是低曝光/ @author Dingger
+ * @param src/ @param enemy_blue 敌人是否是蓝色，默认为真
+ * @return 如果本帧低曝光，返回真
+ */
+bool isLowExposure(cv::Mat &src, const bool enemy_blue = true){
 	cv::Scalar mean_values;
 	cv::Mat channels[3];
 	cv::split(src, channels);
-	cv::Mat gray = channels[0];											//取出蓝色通道
+    cv::Mat gray;
+	if(enemy_blue) gray = channels[0];											//取出蓝色通道
+    else gray = channels[2];                                                    //取出红色通道
     int row_sampling = 16;
     int col_sampling = 16;
     int sampling_sum = 0;
@@ -20,7 +28,7 @@ bool isLowExposure(cv::Mat &src){
         for(int j=0;j<col_sampling;j++){
             int pix_value = gray.at<uchar>(gray.rows*0.1+i*gray.rows*0.8/row_sampling, gray.cols*0.1+j*gray.cols*0.8/col_sampling);
             if(pix_value>30) sampling_sum++;
-            // cv::Point2f smp = cv::Point2f(gray.cols*0.1+i*gray.cols*0.8/col_sampling, gray.rows*0.1+j*gray.rows*0.8/row_sampling);
+            cv::Point2f smp = cv::Point2f(gray.cols*0.1+i*gray.cols*0.8/col_sampling, gray.rows*0.1+j*gray.rows*0.8/row_sampling);
             // cv::circle(src, smp, 3, cv::Scalar(0, 0, 255), -1);
         }
     }
