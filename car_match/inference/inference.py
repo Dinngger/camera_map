9c0b9b5d9f4ca2b88f1ed9906424d07582c4eefc
Dinngger <814089338@qq.com>
2021-03-09 20:00:00 +0800

    add pipe

---
+++ car_match/inference/inference.py
@@ -0,0 +1,60 @@
+
+import os
+import pdb
+import sys
+
+import traceback
+
+import numpy as np
+import tensorflow as tf
+
+module_path = "/home/dinger/mine/RoboMaster/camera_map/"
+input_path = module_path + "fifos/input.pipe"
+output_path = module_path + "fifos/output.pipe"
+model_path = module_path + "car_match/saved_model/"
+
+
+def main():
+    try:
+        os.mkfifo(input_path)
+        os.mkfifo(output_path)
+        print('created pipe file!')
+    except OSError as e:
+        print("mkfifo error: ", e)
+
+    output_f = os.open(input_path, os.O_SYNC | os.O_CREAT | os.O_RDWR)
+    input_f = os.open(output_path, os.O_RDONLY | os.O_NONBLOCK)
+
+    with tf.Session() as sess:
+        meta_graph_def = tf.saved_model.loader.load(sess, ['car_match_model'], model_path)
+        signature = meta_graph_def.signature_def
+        x_tensor = tf.saved_model.utils.get_tensor_from_tensor_info(signature['sig_inout'].inputs['x'])
+        presence_tensor = tf.saved_model.utils.get_tensor_from_tensor_info(signature['sig_inout'].inputs['presence'])
+        belong_tensor = tf.saved_model.utils.get_tensor_from_tensor_info(signature['sig_inout'].outputs['belong'])
+
+        data_string = bytes()
+        while True:
+            data_string += os.read(input_f, 5*13*4)
+            # print('data string len: ', len(data_string))
+            if (len(data_string) >= 5*13*4):
+                data = np.frombuffer(data_string[0:5*13*4], dtype=np.float32, count=5*13)
+                data_string = data_string[5*13*4-1:]
+                print('get data: ', data)
+                data.shape = (1, 13, 5)
+                x = data[:, :, 0:4]
+                presence = data[:, :, 4]
+                belong_pred = sess.run(belong_tensor, feed_dict={x_tensor: x, presence_tensor: presence})
+                os.write(output_f, belong_pred.tobytes())
+                print('send result!')
+    os.close(input_f)
+    os.close(output_f)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as err:
+        last_traceback = sys.exc_info()[2]
+        traceback.print_tb(last_traceback)
+        print(err)
+        pdb.post_mortem(last_traceback)
--- car_match/run.sh
+++ car_match/run.sh
@@ -2,7 +2,7 @@
 python -m model.train\
     --name=car_match\
     --repeat=1000\
-    --batch_size=128\
+    --batch_size=1\
     --save_model=True
     # --overwrite=True
     # --report_loss_steps=1\
